4a0db2766117a981fa0c4b9c7dda37c5260cdbe9
Long Trac <lokesp123@gmail.com>
2024-04-14 13:13:01 -0500

    Connect to a redundancy MySQL server on azure

--- Web Scraper/Web_Scraper.py
+++ Web Scraper/Web_Scraper.py
@@ -20,9 +20,9 @@ import pyodbc
 # Define the server, port, user, password, and database
 server = "teamproj-mysql.database.windows.net"
 port = 1433
-user = "DBUSER"
+user = "DBUSER1"
 password = "ABCd@123"
-database = "TeamProject"
+database = "TeamProjRedundancy"
 
 # Build connection string
 connString = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server},{port};DATABASE={database};UID={user};PWD={password}"
@@ -36,22 +36,12 @@ print("Connected to MySQL!")
 
 
 SQL_QUERY = """
-SELECT 
-TOP 5 c.CustomerID, 
-c.CompanyName, 
-COUNT(soh.SalesOrderID) AS OrderCount 
-FROM 
-SalesLT.Customer AS c 
-LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID 
-GROUP BY 
-c.CustomerID, 
-c.CompanyName 
-ORDER BY 
-OrderCount DESC;
+SELECT  * FROM dbo.NewTable
+
 """
 cursor = mySqlConn.cursor()
 cursor.execute(SQL_QUERY)
 
 records = cursor.fetchall()
 for r in records:
-    print(f"{r.CustomerID}\t{r.OrderCount}\t{r.CompanyName}")
+    print(f"{r}")
