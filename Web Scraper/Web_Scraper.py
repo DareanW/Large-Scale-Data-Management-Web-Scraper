a8061b7e98db619ac1c8b43d81f995941ef2a9b1
Darean Wilde <darean.wilde@my.utsa.edu>
2024-04-14 12:11:03 -0500

    Changed the database connection.

--- Web Scraper/Web_Scraper.py
+++ Web Scraper/Web_Scraper.py
@@ -5,11 +5,11 @@ from pymongo.server_api import ServerApi
 uri = "mongodb+srv://dbUser:LSDM@cluster0.qlhs8tl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+mongoClient = MongoClient(uri, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
+    mongoClient.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
@@ -18,17 +18,40 @@ except Exception as e:
 import pyodbc
 
 # Define the server, port, user, password, and database
-server = "large-scale-data-management-mysql.database.windows.net"
+server = "teamproj-mysql.database.windows.net"
 port = 1433
-user = "dbAdmin"
-password = "Password~"
-database = "MySQL Database"
+user = "DBUSER"
+password = "ABCd@123"
+database = "TeamProject"
 
 # Build connection string
 connString = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server},{port};DATABASE={database};UID={user};PWD={password}"
 
 # Create connection
-conn = pyodbc.connect(connString)
+mySqlConn = pyodbc.connect(connString)
 
 # Print a success message
 print("Connected to MySQL!")
+
+
+
+SQL_QUERY = """
+SELECT 
+TOP 5 c.CustomerID, 
+c.CompanyName, 
+COUNT(soh.SalesOrderID) AS OrderCount 
+FROM 
+SalesLT.Customer AS c 
+LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID 
+GROUP BY 
+c.CustomerID, 
+c.CompanyName 
+ORDER BY 
+OrderCount DESC;
+"""
+cursor = mySqlConn.cursor()
+cursor.execute(SQL_QUERY)
+
+records = cursor.fetchall()
+for r in records:
+    print(f"{r.CustomerID}\t{r.OrderCount}\t{r.CompanyName}")
